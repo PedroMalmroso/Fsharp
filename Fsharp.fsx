11f7b25f135fc34ad33b56d54c6c6b9928bf30da
PedroMalmroso <petermalmroes@gmail.com>
2023-09-04 22:59:27 +0200

    Update Fsharp.fsx

--- Fsharp.fsx
+++ Fsharp.fsx
@@ -1,5 +1,8 @@
-let a = 300
-let b = 400
-let c = a + b
-
-do printfn "%A" c
+let sum x y = x + y
+printfn "Adding a and b"
+printf "Enter a: "
+let a = int (System.Console.ReadLine ())
+printf "Enter b: "
+let b = int (System.Console.ReadLine ())
+let c = sum a b
+do printfn "%A + %A = %A" a b c 
